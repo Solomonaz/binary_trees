65f3771c56e00f076ff5ee909c4680d11a07067f
Solomon <solomonaz2015@gmail.com>
2022-11-14 23:33:30 -0800

    binary tree

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert -
- *
- * Return:
+ * bst_search - search for a special value in the tree and return the node
+ * @tree: tree to go through
+ * @value: value to search
+ * Return: the node with the value or NULL if the value is not in the tree
  */
+
 bst_t *bst_search(const bst_t *tree, int value)
 {
 if (tree == NULL)
